894fa4a54d4bb38a54cfe7f884f522d7c9654df9
Dan Cross <dancross96@gmail.com>
2022-10-05 01:12:39 -0500

    added state update for meme object rather than using regular variables. This will make it easier when using the inouts later

--- src/components/RandomImage.js
+++ src/components/RandomImage.js
@@ -2,15 +2,26 @@ import memesData from './memesData.js'
 import React from "react"
 
 export default function Meme() {
-  const [memeImage, setMemeImage] = React.useState("")
+  // const [memeImage, setMemeImage] = React.useState("")
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg"
+  })
+
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
   function getMemeImage() {
-    const memesArray = memesData.data.memes
+    const memesArray = allMemeImages.data.memes
     const randomNumber = Math.floor(Math.random() * memesArray.length)
-    setMemeImage(memesArray[randomNumber].url)
+    const url = memesArray[randomNumber].url
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      randomImage: url
+    }))
   }
   return <div className="LastButton">
     <button onClick={getMemeImage} className="generate">Create a new meme image 🖼</button>
-    <img className="RandomImage" src={memeImage} alt="Meme" />
+    <img className="RandomImage" src={meme.randomImage} alt="Meme" />
   </div>
 }
